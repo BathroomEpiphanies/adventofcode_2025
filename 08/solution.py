73a1fb4610bb65a40acf3086846065f079038412
Fredrik Atmer <fredrik@atmer.se>
2025-12-10 08:59:45 +0100

    Satisfying mypy for December 8th.

--- 08/solution.py
+++ 08/solution.py
@@ -1,31 +1,41 @@
 from io import TextIOWrapper
-from typing import NamedTuple,Self
+from typing import Self
 
 from functools import reduce
 from itertools import combinations
 from operator import mul
 
 
-class Point(NamedTuple):
+class CoordinateXYZ:
     x:int
     y:int
     z:int
+    def __init__(self, x:int, y:int, z:int) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+    def __str__(self) -> str:
+        return f'({self.x},{self.y},{self.z})'
+    def __add__(self, other:Self) -> Self:
+        return self.__class__(self.x+other.x, self.y+other.y, self.z+other.z)
     def __sub__(self, other:Self) -> Self:
-        return Point(self.x-other.x, self.y-other.y, self.z-other.z)
+        return self.__class__(self.x-other.x, self.y-other.y, self.z-other.z)
     def __abs__(self) -> int:
         return self.x**2+self.y**2+self.z**2
+    def __hash__(self) -> int:
+        return hash((self.x,self.y,self.z))
 
 
-def parse_input(file_handle:TextIOWrapper) -> list[Point]:
-    return [Point(*map(int, line.strip().split(','))) for line in file_handle.readlines()]
+def parse_input(file_handle:TextIOWrapper) -> list[CoordinateXYZ]:
+    return [CoordinateXYZ(*map(int, line.strip().split(','))) for line in file_handle.readlines()]
 
 
-def connect_boxes(boxes, max_iterations=0):
-    circuits = {p:frozenset([p]) for p in boxes}
-    distances = sorted((abs(a-b),a,b) for a,b in combinations(circuits.keys(), 2))
+def connect_boxes(boxes:list[CoordinateXYZ], max_iterations:int=0) -> tuple[set[frozenset[CoordinateXYZ]],CoordinateXYZ,CoordinateXYZ]:
+    circuits:dict[CoordinateXYZ,frozenset[CoordinateXYZ]] = {p:frozenset([p]) for p in boxes}
+    distances:list[tuple[int,CoordinateXYZ,CoordinateXYZ]] = sorted((abs(a-b),a,b) for a,b in combinations(circuits.keys(), 2))
     if max_iterations>0:
         distances = distances[:max_iterations]
-    for d,a,b in distances:
+    for _,a,b in distances:
         c = circuits[a]|circuits[b]
         for d in circuits[a]:
             circuits[d] = c
@@ -36,13 +46,13 @@ def connect_boxes(boxes, max_iterations=0):
     return set(circuits.values()),a,b
 
 
-def part1(problem_input:list[Point]) -> int:
+def part1(problem_input:list[CoordinateXYZ]) -> int:
     iterations = 10 if len(problem_input)<1000 else 1000
     circuits,*_ = connect_boxes(problem_input, max_iterations=iterations)
     largest_circuits = sorted(circuits, key=lambda c: len(c))[-3::]
     return reduce(mul, (len(c) for c in largest_circuits))
 
 
-def part2(problem_input:list[Point]) -> int:
+def part2(problem_input:list[CoordinateXYZ]) -> int:
     _,a,b = connect_boxes(problem_input)
     return a.x*b.x
